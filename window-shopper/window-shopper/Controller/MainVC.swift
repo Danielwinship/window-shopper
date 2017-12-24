b6591ad6191c351ca8d5ffe58faea96b89ba5ecf
Daniel Winship <danielwinship@icloud.com>
2017-12-24 07:21:50 -0500

    created wage and price text fields and wired up calculate button to keyboard

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var priceTextField: CurrencyTextField!
+    @IBOutlet weak var wageTextField: CurrencyTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y:0, width:view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(.white, for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        wageTextField.inputAccessoryView = calcButton
+        priceTextField.inputAccessoryView = calcButton
+        
     }
 
+  @objc func calculate() {
+        print("we got here")
+    }
   
 
 
